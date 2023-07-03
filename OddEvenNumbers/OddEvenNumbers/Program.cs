18d2987b34a728e5327f4fc3cb568575fb3f1596
Waleed <Waleed716W@gmail.com>
2023-07-03 19:49:49 +0300

    Initial Commit

---
+++ OddEvenNumbers/OddEvenNumbers/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace OddEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            PrintNumbers("Numbers", numbers);
+
+            PrintNumbers("Even Numbers", numbers.Where(num => IsEven(num)));
+            PrintNumbers("Odd Numbers", numbers.Where(num => IsOdd(num)));
+
+
+        }
+
+        static void PrintNumbers(string title , IEnumerable<int> numbers)
+        {
+            Console.WriteLine();
+
+            Console.Write($"{title}: [");
+            foreach (int number in numbers)
+            {
+                Console.Write($" {number}");
+            }
+            Console.Write(" ]");
+
+            Console.WriteLine();
+        }
+
+        static bool IsEven(int number)
+        {
+            if (number % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static bool IsOdd(int number)
+        {
+            if (number % 2 != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
